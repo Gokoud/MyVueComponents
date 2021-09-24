1f9488ee5a440ea4438dd8d7a994e67d2d4953dd
李茂森 <1093890786@qq.com>
2021-09-24 21:19:45 +0800

    自己练手的小项目

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <ms-header :list="list" />
+  </div>
+</template>
+
+<script>
+import msHeader from '@/components/msHeader/msHeader'
+export default {
+  components: {
+    msHeader
+  },
+  data() {
+    return {
+      list: [
+        {name: '首页'},
+        {name: '个人中心'},
+        {name: '关于'}
+      ]
+    }
+  }
+}
+</script>
+<style>
+@import url('./assets/common/common.css');
+</style>
---
+++ src/components/msHeader/msHeader.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="header flex end-center bottom-shadow">
+      <div class="nav flex center-center" v-for="(nav, key) in list" :key="key">
+        <p>{{nav.name}}</p>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Ms-Header',
+  props: {
+    list:{
+      type: Array,
+      value: []
+    }
+  }
+}
+</script>
+
+<style scoped>
+.header {
+  border-radius: 10px;
+  border: 1px solid #eeeeee;
+}
+.nav {
+  height: 50px;
+  width: 80px;
+  border-radius: 10px;
+}
+.nav:hover {
+  box-shadow: inset 3px 1px 3px #ebebeb;
+}
+</style>
